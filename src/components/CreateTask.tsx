366fa423548fc06a8811d23db7f33354d12e98f7
Jefferson Vinicius <vinicius.felipe1319@gmail.com>
2026-02-25 20:51:31 -0300

    refactor(CreateTask): improve CreateTask semantics and add form validation

--- src/components/CreateTask.tsx
+++ src/components/CreateTask.tsx
@@ -10,30 +10,37 @@ export function CreateTask() {
       >
         Create Task
       </h2>
-      <input
-        type="text"
-        placeholder="Enter the title..."
-        aria-label="Enter the task title"
-        className="h-9 w-full rounded-md border-2 border-slate-300 bg-white px-2.5 transition-colors outline-none placeholder:text-slate-400 placeholder:italic focus:border-slate-500"
-      />
-      <select
-        aria-label="Task category"
-        defaultValue=""
-        className="h-9 w-full cursor-pointer rounded-md border-2 border-slate-300 bg-white px-1.5 text-sm transition-colors outline-none focus:border-slate-500"
-      >
-        <option value="" disabled hidden>
-          Select a category
-        </option>
-        <option value="studies">Studies</option>
-        <option value="work">Work</option>
-        <option value="personal">Personal</option>
-      </select>
-      <button
-        aria-controls="task-list"
-        className="w-full cursor-pointer rounded-lg bg-indigo-700 px-4 py-3 font-medium text-white transition-all hover:bg-indigo-800 active:scale-[0.98] md:w-fit md:self-start"
-      >
-        Create Task
-      </button>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          required
+          name="taskTitle"
+          placeholder="Enter the title..."
+          aria-label="Enter the task title"
+          className="h-9 w-full rounded-md border-2 border-slate-300 bg-white px-2.5 transition-colors outline-none placeholder:text-slate-400 placeholder:italic focus:border-slate-500"
+        />
+        <select
+          required
+          name="taskCategory"
+          aria-label="Task category"
+          defaultValue=""
+          className="h-9 w-full cursor-pointer rounded-md border-2 border-slate-300 bg-white px-1.5 text-sm transition-colors outline-none focus:border-slate-500"
+        >
+          <option value="" disabled hidden>
+            Select a category
+          </option>
+          <option value="studies">Studies</option>
+          <option value="work">Work</option>
+          <option value="personal">Personal</option>
+        </select>
+        <button
+          type="submit"
+          aria-controls="task-list"
+          className="w-full cursor-pointer rounded-lg bg-indigo-700 px-4 py-3 font-medium text-white transition-all hover:bg-indigo-800 active:scale-[0.98] md:w-fit md:self-start"
+        >
+          Create Task
+        </button>
+      </form>
     </section>
   );
 }
