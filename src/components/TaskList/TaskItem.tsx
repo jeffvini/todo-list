01b2e57d96364a9676f91b6d85eee6eeb27ee546
Jefferson Vinicius <vinicius.felipe1319@gmail.com>
2026-03-01 22:20:34 -0300

    refactor(TaskItem): receive full task object instead of individual props

--- src/components/TaskList/TaskItem.tsx
+++ src/components/TaskList/TaskItem.tsx
@@ -1,11 +1,8 @@
+import type { TaskItemProps } from '../../interfaces/TaskItemProps';
+
 import { Trash2 } from 'lucide-react';
 
-interface TaskItemProps {
-  title: string;
-  category: string;
-}
-
-export function TaskItem({ title, category }: TaskItemProps) {
+export function TaskItem({ task }: TaskItemProps) {
   return (
     <li className="flex items-center justify-between gap-5 rounded-md bg-white px-4 py-6 shadow-[0_0_7px_4px_rgb(0,0,0,0.1)]">
       <label className="flex flex-1 cursor-pointer items-center gap-5">
@@ -14,9 +11,9 @@ export function TaskItem({ title, category }: TaskItemProps) {
           className="h-4 w-4 cursor-pointer transition-transform active:scale-95"
         />
         <div className="flex flex-1 flex-col gap-2">
-          <p className="text-lg text-slate-800">{title}</p>
+          <p className="text-lg text-slate-800">{task.title}</p>
           <span className="text-xs font-bold tracking-widest text-slate-500 uppercase">
-            ({category})
+            ({task.category})
           </span>
         </div>
       </label>
