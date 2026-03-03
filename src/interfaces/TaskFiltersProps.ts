1db7f45d0bdc1466b915b9404473953b50b7b310
Jefferson Vinicius <vinicius.felipe1319@gmail.com>
2026-03-02 22:33:48 -0300

    chore(interfaces): define SortOrder and TaskFilters interfaces for sorting logic

--- src/interfaces/TaskFiltersProps.ts
+++ src/interfaces/TaskFiltersProps.ts
@@ -1,6 +1,8 @@
 export interface TaskFiltersProps {
   search: string;
   filterStatus: 'all' | 'completed' | 'pending';
+  sortOrder: 'asc' | 'desc';
   onChangeSearch: (value: string) => void;
   onChangeStatus: (status: 'all' | 'completed' | 'pending') => void;
+  onChangeSort: (sort: 'asc' | 'desc') => void;
 }
