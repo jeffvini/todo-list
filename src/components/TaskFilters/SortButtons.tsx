4044fdcac88ded1560e80e86df7b8f803d392604
Jefferson Vinicius <vinicius.felipe1319@gmail.com>
2026-03-02 22:44:14 -0300

    feat(SortButtons): implement SortButtons with type-safe logic, ARIA accessibility, and dynamic states

--- src/components/TaskFilters/SortButtons.tsx
+++ src/components/TaskFilters/SortButtons.tsx
@@ -1,4 +1,14 @@
-export function SortButtons() {
+import type { SortButtonsProps } from '../../interfaces/SortButtonsProps';
+
+export function SortButtons({ sortOrder, onChangeSort }: SortButtonsProps) {
+  const handleChangeSort = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const newValue = e.currentTarget.value;
+
+    if (newValue === 'asc' || newValue === 'desc') {
+      onChangeSort(newValue);
+    }
+  };
+
   return (
     <div className="flex flex-1 flex-col gap-3">
       <span className="text-xs font-bold tracking-wider text-slate-700 uppercase">
@@ -7,19 +17,23 @@ export function SortButtons() {
       <div className="flex gap-2">
         <button
           type="button"
-          aria-pressed="true"
+          value="asc"
+          onClick={handleChangeSort}
+          aria-pressed={sortOrder === 'asc'}
           aria-label="Sort tasks by name in ascending order"
           aria-controls="task-list"
-          className="h-9 flex-1 cursor-pointer rounded-md border-2 border-slate-700 bg-slate-700 px-3 text-xs font-bold whitespace-nowrap text-white transition-all active:scale-95"
+          className={`h-9 flex-1 cursor-pointer rounded-md border-2 px-3 ${sortOrder === 'asc' ? 'border-slate-700 bg-slate-700 text-white' : 'border-transparent bg-slate-200 hover:border-slate-400 hover:text-slate-700'} text-xs font-bold whitespace-nowrap text-slate-500 transition-all active:scale-95`}
         >
           A-Z
         </button>
         <button
           type="button"
-          aria-pressed="false"
+          value="desc"
+          onClick={handleChangeSort}
+          aria-pressed={sortOrder === 'desc'}
           aria-label="Sort tasks by name in descending order"
           aria-controls="task-list"
-          className="h-9 flex-1 cursor-pointer rounded-md border-2 border-transparent bg-slate-200 px-3 text-xs font-bold whitespace-nowrap text-slate-500 transition-all hover:border-slate-400 hover:text-slate-700 active:scale-95"
+          className={`h-9 flex-1 cursor-pointer rounded-md border-2 px-3 ${sortOrder === 'desc' ? 'border-slate-700 bg-slate-700 text-white' : 'border-transparent bg-slate-200 hover:border-slate-400 hover:text-slate-700'} text-xs font-bold whitespace-nowrap text-slate-500 transition-all active:scale-95`}
         >
           Z-A
         </button>
